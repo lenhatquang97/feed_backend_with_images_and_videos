0588539688b81a426ffff6ab0741ce152c5778bb
p0tluck <lenhatquang97@gmail.com>
2022-09-16 06:49:45 +0700

    AWS implementation

--- src/main.go
+++ src/main.go
@@ -1,6 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
+	fmt.Println(GetOutboundIP())
 	ConnectDB()
 	initializeAPI()
+
+}
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
